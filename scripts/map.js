441a524d57144671be78c6f2d4c9efe211abefc0
SandraSon1 <sandra.thi.son@gmail.com>
2018-05-26 14:00:07 +1200

    test

---
+++ scripts/map.js
@@ -0,0 +1,60 @@
+var map;
+
+function initialize() {
+
+    var mapOptions = {
+          center: {lat: -41.2865, lng: 174.7762},
+          zoom: 15
+        };
+      
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Create the DIV to hold the control and call the constructor passing in this DIV
+    var geolocationDiv = document.createElement('div');
+    var geolocationControl = new GeolocationControl(geolocationDiv, map);
+
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(geolocationDiv);
+}
+
+function GeolocationControl(controlDiv, map) {
+
+        var controlUI = document.getElementById('mapbutt');
+
+        google.maps.event.addDomListener(controlUI, 'click', geolocate);
+}
+
+function geolocate() {
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+
+            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            // Create a marker and center map on user location
+            marker = new google.maps.Marker({
+                position: pos,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                map: map
+
+            });
+
+            map.setCenter(pos);
+
+            document.getElementById("myNav").style.width = "0%";
+            document.getElementById("mapbutt").style.display = "none";
+            document.getElementById("startWalking").style.display = "none";
+
+     
+
+        });
+    } 
+
+    else {
+         document.getElementById("startWalking").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+initialize();
